0a51b525f9f7677b081801a41e7b6f146bc1bf82
Stogdy <67536549+Stogdy@users.noreply.github.com>
2025-02-24 11:34:23 +0100

    commit 5

---
+++ m3prog_project/public/01/its-me.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Favoriete film: The Green Book, ";
+echo "Favoriete game: Dark Souls III,";
+echo " ,SD1A";
---
+++ m3prog_project/public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<img src='images/shikanoko.png' width='500px' alt=''>"; 
+
+?>
