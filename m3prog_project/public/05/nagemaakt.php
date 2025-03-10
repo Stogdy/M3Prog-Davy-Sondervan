2fb8f1ab8e6ecfcdfee535622fc54aef1bf0f4cd
Stogdy <67536549+Stogdy@users.noreply.github.com>
2025-03-10 14:17:09 +0100

    commit 14

---
+++ m3prog_project/public/05/nagemaakt.php
@@ -0,0 +1,15 @@
+<?php
+
+function mijnPrint()
+{
+print("mijnPrint");
+print("<br>");
+}
+
+mijnPrint("regel 1");
+
+mijnPrint("regel 2");
+
+mijnPrint("regel 3");
+
+mijnPrint("vraag 1) lees de fout en bedenk je WAAROM het fout gaat, schrijf dit in commentaar in je code");
