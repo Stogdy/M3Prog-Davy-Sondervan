f01348508a037c94807bbe86408025bfa08617ab
Stogdy <67536549+Stogdy@users.noreply.github.com>
2025-03-17 13:06:42 +0100

    commit 17

--- m3prog_project/public/05/Returns.php
+++ m3prog_project/public/05/Returns.php
@@ -6,3 +6,26 @@ return 1;
 
 $mijnFunctionResultaat = mijnFunction();
 print($mijnFunctionResultaat);
+
+echo "<br>";
+
+function returnVariable()
+{
+        $result = rand();
+        return $result;
+}
+print(returnVariable());
+print("<br>");
+function returnVariable2()
+{
+        $result = rand();
+        return $result;
+}
+print(returnVariable2());
+print("<br>");
+function returnVariable3()
+{
+        $result = rand();
+        return $result;
+}
+print(returnVariable3());
